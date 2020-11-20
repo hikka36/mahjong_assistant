120a38526d77875fa4268b2e0218555dda1367e0
Hikaru Hiraoka <pirapikamon5@icloud.com>
2020-11-20 17:58:18 +0900

    Initial Commit

---
+++ mahjong_assistant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mahjong_assistant
+//
+//  Created by 平岡煌琉 on 2020/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
